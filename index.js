edb2bfae271876fe29e1b8237e629f3a954f4368
Rodrigo Sonego <rodrigoasonego@gmail.com>
2021-04-13 17:40:48 -0300

    feature: enemies, projectiles and player collision update: fix canvas size

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ game.restart = () => {};
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+var isStopped;
 
 class Vector2 {
   x;
@@ -94,10 +93,11 @@ class Enemy {
 const midX = canvas.width / 2;
 const midY = canvas.height / 2;
 
-const player = new Player(midX, midY, 20, 'blue');
+const player = new Player(midX, midY, 20, 'white');
 
 var projectiles = [];
 var enemies = [];
+var animationID;
 
 function getAngleToMidScreen(x,y) {
   const yDistance = y - midY;
@@ -110,21 +110,15 @@ function getRandomBetween(min,max) {
   return Math.random() * (max - min) + min;
 }
 
-function checkDistanceToProjectiles(enemy) {
-  projectiles.forEach(projectile => {
-    const dist = Vector2.distance(projectile.position, enemy.position);
-    console.log(dist);
-  });
-}
 function spawnEnemies() {
-
+  
   setInterval(() => {
     const radius = getRandomBetween(9,20);
     const color = 'green';
-
+    
     var x;
     var y;
-
+    
     if(Math.random() < 0.5) {
       x = Math.random() < 0.5 ? 0-radius : canvas.width+radius;
       y = Math.random() * canvas.height;
@@ -132,48 +126,110 @@ function spawnEnemies() {
       x = Math.random() * canvas.width;
       y = Math.random() < 0.5 ? 0-radius : canvas.height+radius;
     }
-
-
+    
+    
     const angle = Math.atan2(midY - y, midX - x);
-
+    
     const velocity = new Vector2(Math.cos(angle), Math.sin(angle));
-
+    
     const position = new Vector2(x,y);
     var enemy = new Enemy(position,radius,color,velocity);
-
+    
     enemies.push(enemy);
   }, 1000 );
-
+  
 }
 
-function animate() {
-  requestAnimationFrame(animate);
+function checkDistanceToProjectiles(enemy, enIndex) { 
+  projectiles.forEach((projectile, prIndex ) => {
+    const dist = Vector2.distance(projectile.position, enemy.position);
 
-  context.clearRect(0,0, canvas.width, canvas.height);
-  player.draw();
-
-  projectiles.forEach( projectile => {
-    projectile.update();
+    if(dist - enemy.radius - projectile.radius < 1){
+      setTimeout(() => { //previne um "flash" quando inimigo é destruído
+        projectiles.splice(prIndex,1);
+        enemies.splice(enIndex,1);
+      }, 0)
+    }
   });
+}
 
-  enemies.forEach(enemy => {
+function updateEnemies() {
+  enemies.forEach((enemy, index) => {
     enemy.update();
-    checkDistanceToProjectiles(enemy);
+    const distToPlayer = Vector2.distance(player.position, enemy.position);
+    
+    if(distToPlayer - enemy.radius - player.radius < 0.7) {
+      cancelAnimationFrame(animationID);
+      isStopped = true;
+    }
+
+    checkDistanceToProjectiles(enemy, index);
   })
 }
 
-addEventListener('click', (event) => {
-  const angle = getAngleToMidScreen(event.clientX, event.clientY);
+function updateProjectiles() {
+  projectiles.forEach((projectile, index) => {
+    projectile.update();
 
+    if(projectile.position.x - projectile.radius < 0 ||
+       projectile.position.x - projectile.radius > canvas.width ||
+       projectile.position.y + projectile.radius < 0 ||
+       projectile.position.y - projectile.radius > canvas.height) {
+      setTimeout(() => {
+        projectiles.splice(index,1);
+      }, 0)
+    }
+  });
+}
+
+
+function animate() {
+  animationID = requestAnimationFrame(animate);
+  context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // context.clearRect(0,0, canvas.width, canvas.height);
+  player.draw();
+  
+  updateProjectiles();
+  updateEnemies();
+
+}
+
+function restartGame(){
+  projectiles = [];
+
+  enemies = [];
+
+  animate();
+}
+
+function onMouseClick(event) {
+  console.log(projectiles);
+  const angle = getAngleToMidScreen(event.clientX, event.clientY);
   const velocity = new Vector2();
-  velocity.x = Math.cos(angle);
-  velocity.y = Math.sin(angle);
+  velocity.x = Math.cos(angle) * 4;
+  velocity.y = Math.sin(angle) * 4;
 
   const projectile = new Projectile(new Vector2(midX,midY),
-    7, 'red', velocity);
+    7, 'white', velocity);
 
   projectiles.push(projectile);
+}
+
+addEventListener('click', (event) => onMouseClick(event));
+
+addEventListener('keypress', (event) => {
+  console.log(event.key);
+  if(event.key == 's') {
+    cancelAnimationFrame(animationID);
+    restartGame();
+  }
 })
 
-animate();
-spawnEnemies();
+if(!isStopped){
+  animate();
+  spawnEnemies();
+} else {
+  cancelAnimationFrame(animationID);
+}
