9fae9a4139be6588bb3f2f115a36d2a0d029d9a2
Rodrigo Sonego <rodrigoasonego@gmail.com>
2021-04-13 17:49:29 -0300

    feature: randomize enemy colors

--- index.js
+++ index.js
@@ -114,7 +114,9 @@ function spawnEnemies() {
   
   setInterval(() => {
     const radius = getRandomBetween(9,20);
-    const color = 'green';
+
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const color = '#' + randomColor;
     
     var x;
     var y;
